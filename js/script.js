6bc318f01e846519d13cac549657c8eb93a6e5c0
ThanhTT <thanhtt@splus-software.com.vn>
2024-12-31 21:47:56 +0700

    m

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.my-banner').slick({
+        infinite: false,
+        dots: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 200000,
+    });
+  });
